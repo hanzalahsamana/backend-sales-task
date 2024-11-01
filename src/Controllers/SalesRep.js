778ad23689fb3db7f0b4d0c40bf7841ac5dcca5c
hanzalahsamana789 <hanzalahsamana789@gmail.com>
2024-11-01 21:56:20 +0500

    remove extra commits

--- src/Controllers/SalesRep.js
+++ src/Controllers/SalesRep.js
@@ -4,14 +4,12 @@ const getSalesRep = async (req, res) => {
     try {
         const countries = await Country.find();
 
-        // Group countries by region
         const regions = countries.reduce((acc, { region, name }) => {
             if (!acc[region]) acc[region] = [];
             acc[region].push(name);
             return acc;
         }, {});
 
-        // Assign countries to representatives
         const representatives = Object.entries(regions).reduce((acc, [region, countries]) => {
             acc[region] = splitCountriesIntoReps(countries);
             return acc;
@@ -24,7 +22,6 @@ const getSalesRep = async (req, res) => {
     }
 };
 
-// Function to split an array of countries into representatives
 const splitCountriesIntoReps = (countries) => {
     const reps = {};
     let repIndex = 1;
@@ -33,7 +30,7 @@ const splitCountriesIntoReps = (countries) => {
         const numCountries = Math.min(7, Math.max(3, Math.floor(Math.random() * countries.length) + 1));
 
         if (countries.length - numCountries < 3 && countries.length - numCountries !== 0) {
-            continue; // Skip if fewer than 3 countries would remain
+            continue;
         } else {
             reps[`Rep${repIndex}`] = countries.splice(0, numCountries);
             repIndex++;
